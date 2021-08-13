325f92b8062f94007bd08b42acd61a1f3a8d6a68
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-08-13 18:15:40 +0200

    Add full script to it.

--- script.js
+++ script.js
@@ -24,4 +24,30 @@ body.append(div);
 div.innerText="hello world";
 div.textContent = "hello wereld 2"
 */
+const spottedAnimalButtons = document.querySelectorAll('.big-five-button');
+const removeFirstAnimalButton = document.getElementById('remove-first-item-button');
+const removeAllAnimalsButton = document.getElementById('remove-all-button');
 
+spottedAnimalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const newSpottedAnimal = document.getElementById('spotted-animals-list');
+    const newLi = document.createElement('li');
+    newLi.className = 'spotted-animals-list-item';
+    newSpottedAnimal.appendChild(newLi);
+    newLi.innerText = button.innerHTML;
+  });
+});
+
+removeFirstAnimalButton.addEventListener('click', () => {
+  let list = document.getElementById('spotted-animals-list');
+  if (list.hasChildNodes()) {
+    list.removeChild(list.childNodes[0]);
+  }
+});
+
+removeAllAnimalsButton.addEventListener('click', () => {
+  let list = document.getElementById('spotted-animals-list');
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+});
