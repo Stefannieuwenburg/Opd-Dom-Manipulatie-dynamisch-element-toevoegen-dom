1e33f0e4d6c4e9a3524cf74aa5e7512e95ebbbef
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-08-13 17:59:10 +0200

    Replace full html index

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+//stappenplan
+//stap 1 alles mogelijk li-div-h1-input-button enz.
+const elem = document.createElement("div");
+// stap 2
+document.body.appendChild(elem);
+//eind
+//extra opties zijn.
+elem.appendChild(elemtext);
+const elemtext = document.createTextNode("this is a div");
+elem.classList.add("container");
+elem.id = "box-1";
+parent.removeChild(child)
+
+//optie 2
+//stap 1
+const body = document.body
+//stap2
+const div = document.createElement("div");
+//stap 3
+body.append(div);
+//stap 4
+//extra opties zijn.
+div.innerText="hello world";
+div.textContent = "hello wereld 2"
+*/
+
